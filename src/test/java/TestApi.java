067505476ede1e53244ea5335f0eb114db00226b
Oleksandr Golubishko <oleksandr.golubishko@ab-soft.net>
2023-04-09 14:44:11 +0300

    Add more API tests

--- src/test/java/TestApi.java
+++ src/test/java/TestApi.java
@@ -1,22 +1,54 @@
+import io.qameta.allure.Step;
 import io.restassured.RestAssured;
+import org.aspectj.lang.annotation.Before;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class TestApi {
-    @Test
-    public void createUser() {
-        RestAssured.baseURI = "https://qauto.forstudy.space/api/";
 
+    @BeforeMethod
+    public void setup() {
+        RestAssured.baseURI = "https://qauto.forstudy.space/api/";
+    }
+
+    @Test
+    public void createUserWithValidData() {
+        createUser();
+    }
+
+    @Test
+    public void loginToAccount() {
+        String email = createUser();
+        logIn(email);
+    }
+
+    @Step
+    private String createUser() {
+        String randomEmail = randomEmail();
         given().log().all().header("content-type", "application/json")
                 .body("{\n" +
                         "  \"name\": \"John\",\n" +
                         "  \"lastName\": \"Dou\",\n" +
-                        "  \"email\": \" " + randomEmail() + "\",\n" +
+                        "  \"email\": \" " + randomEmail + "\",\n" +
                         "  \"password\": \"Qwerty12345\",\n" +
                         "  \"repeatPassword\": \"Qwerty12345\"\n" +
                         "}").
                 when().post("auth/signup").
+                then().log().all().assertThat().statusCode(201);
+        return randomEmail;
+    }
+
+    @Step
+    private void logIn(String email){
+        given().log().all().header("content-type", "application/json")
+                .body("{\n" +
+                        "  \"email\": \" " + email + "\",\n" +
+                        "  \"password\": \"Qwerty12345\",\n" +
+                        "  \"remember\": false\n" +
+                        "}").
+                when().post("auth/signin").
                 then().log().all().assertThat().statusCode(201);
     }
 
