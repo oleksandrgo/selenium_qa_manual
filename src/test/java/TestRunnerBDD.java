0d07b5104764ec5e143e920addf352111811981f
Oleksandr Golubishko <oleksandr.golubishko@ab-soft.net>
2023-05-25 16:54:52 +0300

    Add monochrome

--- src/test/java/TestRunnerBDD.java
+++ src/test/java/TestRunnerBDD.java
@@ -5,7 +5,8 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/java",
         glue = {"stepDefinitionBDD"},
-        plugin = {"pretty","html:target/CucumberReport.html","json:target/report.json"}
+        plugin = {"pretty","html:target/CucumberReport.html","json:target/report.json"},
+        monochrome = true
 )
 public class TestRunnerBDD extends AbstractTestNGCucumberTests {
 
